a755b365f9a41009aafa9570da29ee33b64ea78a
Juando26030 <juando02603@gmail.com>
2025-11-03 13:32:06 -0500

    feat: implementar API REST con Swagger para todas las entidades

---
+++ personapi-dotnet/Controllers/Api/TelefonosApiController.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using personapi_dotnet.Models.Entities;
+
+namespace personapi_dotnet.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TelefonosApiController : ControllerBase
+    {
+        private readonly PersonaDbContext _context;
+
+        public TelefonosApiController(PersonaDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TelefonosApi
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Telefono>>> GetTelefonos()
+        {
+            return await _context.Telefonos.ToListAsync();
+        }
+
+        // GET: api/TelefonosApi/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Telefono>> GetTelefono(string id)
+        {
+            var telefono = await _context.Telefonos.FindAsync(id);
+
+            if (telefono == null)
+            {
+                return NotFound();
+            }
+
+            return telefono;
+        }
+
+        // PUT: api/TelefonosApi/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTelefono(string id, Telefono telefono)
+        {
+            if (id != telefono.Num)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(telefono).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TelefonoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TelefonosApi
+        [HttpPost]
+        public async Task<ActionResult<Telefono>> PostTelefono(Telefono telefono)
+        {
+            _context.Telefonos.Add(telefono);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (TelefonoExists(telefono.Num))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetTelefono", new { id = telefono.Num }, telefono);
+        }
+
+        // DELETE: api/TelefonosApi/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTelefono(string id)
+        {
+            var telefono = await _context.Telefonos.FindAsync(id);
+            if (telefono == null)
+            {
+                return NotFound();
+            }
+
+            _context.Telefonos.Remove(telefono);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TelefonoExists(string id)
+        {
+            return _context.Telefonos.Any(e => e.Num == id);
+        }
+    }
+}
+
--- personapi-dotnet/Program.cs
+++ personapi-dotnet/Program.cs
@@ -1,14 +1,48 @@
 using personapi_dotnet.Models.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using personapi_dotnet.Controllers.Api;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+
+
+
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<PersonasApiController>();
+builder.Services.AddScoped<EstudiosApiController>();
+builder.Services.AddScoped<TelefonosApiController>();
+
+
+else
+{
+
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+    });
+
+}
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Persona API", Version = "v1" });
+});
 
 builder.Services.AddDbContext<PersonaDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.ListenAnyIP(8080);
 });
 
 var app = builder.Build();
